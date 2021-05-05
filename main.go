59a63a3ec46f14a8dd3678e8a0d494eff6d50d48
tdrk18 <s18.todoroki@gmail.com>
2021-05-05 20:20:50 +0900

    Get response from status endpoint

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	ENDPOINT = "https://kctbh9vrtdwd.statuspage.io/api/v2/status.json"
+)
 
 func main() {
-	fmt.Println("Hello")
+	data := getStatus()
+	fmt.Println(data)
 }
 
 type GitHubStatus struct {
@@ -22,4 +32,19 @@ type Page struct {
 type Status struct {
 	Indicator   string `json:"indicator"`
 	Description string `json:"description"`
+}
+
+func getStatus() GitHubStatus {
+	req, _ := http.NewRequest("GET", ENDPOINT, nil)
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data GitHubStatus
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
 }
