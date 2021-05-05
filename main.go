c6ec1c7555b66b351ce1f23fd89ad0902ba2e148
tdrk18 <s18.todoroki@gmail.com>
2021-05-05 20:20:04 +0900

    Add structs for response

--- main.go
+++ main.go
@@ -5,3 +5,21 @@ import "fmt"
 func main() {
 	fmt.Println("Hello")
 }
+
+type GitHubStatus struct {
+	Page   Page   `json:"page"`
+	Status Status `json:"status"`
+}
+
+type Page struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Url       string `json:"url"`
+	Timezone  string `json:"time_zone"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type Status struct {
+	Indicator   string `json:"indicator"`
+	Description string `json:"description"`
+}
